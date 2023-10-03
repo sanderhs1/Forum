b0bccde4c2603f52e1b66c59ea31e65977b85458
sanderhs1 <91096641+sanderhs1@users.noreply.github.com>
2023-10-03 05:30:10 +0200

    idk

---
+++ Forum/Forum/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
