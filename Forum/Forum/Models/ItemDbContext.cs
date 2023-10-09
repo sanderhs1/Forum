d9adbcc2a30baf83f3a1012eb30aadd3cea2ca8d
s356291 <89486176+s356291@users.noreply.github.com>
2023-10-09 18:23:00 +0200

    Lagt til en Database

---
+++ Forum/Forum/Models/ItemDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Models;
+
+
+public class ItemDbContext : DbContext
+{
+    public ItemDbContext(DbContextOptions<ItemDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+    public DbSet<Item> Items { get; set; }
+}
