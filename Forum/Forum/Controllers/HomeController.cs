ffbbec65a39b56654e592ef73a056a4dda507570
Tony Nguyen <s341492@oslomet.no>
2023-10-12 18:53:44 +0200

    lagt til kommentar på HomeController

--- Forum/Forum/Controllers/HomeController.cs
+++ Forum/Forum/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Forum.Controllers
+namespace Forum.Controllers 
 {
+    //This controller class called HomeController will handle requests and responses 
     public class HomeController : Controller
     {
+        //When users visit the site´s home page this this method will return the content to be displayed in the user´s web browser
         public IActionResult Index()
         {
             return View();
