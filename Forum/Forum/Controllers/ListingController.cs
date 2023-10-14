7758427378c3a95dd566295c501c62f075a8832f
s356291 <89486176+s356291@users.noreply.github.com>
2023-10-14 12:16:18 +0200

    Lagt til errorhandling i Controller.

--- Forum/Forum/Controllers/ListingController.cs
+++ Forum/Forum/Controllers/ListingController.cs
@@ -10,19 +10,26 @@ namespace Forum.Controllers
     public class ListingController : Controller
     {
         private readonly ListingDbContext _listingDbContext;
+        private readonly ILogger<ListingController> _logger;
 
-        public ListingController(ListingDbContext listingDbContext)
+        public ListingController(ListingDbContext listingDbContext, ILogger<ListingController> logger)
         {
+            _logger = logger;
             _listingDbContext = listingDbContext;
         }
 
         public List<Rent> RentConsole()
         {
             return _listingDbContext.Rents.ToList();
+
         }
+        
 
         public async Task<IActionResult>Table()
         {
+            _logger.LogInformation("This is an information message");
+            _logger.LogWarning("This is a warning message");
+            _logger.LogError("This is an error message");
             List<Listing> listings = await _listingDbContext.Listings.ToListAsync();
             var listingListViewModel = new ListingListViewModel(listings, "Table");
             return View(listingListViewModel);
@@ -30,7 +37,13 @@ namespace Forum.Controllers
         // Fjerne Grid/Table View, Endre til ett view, hvor vi stacker listing bokser oppå hverandre, ikke i et table, men table kan brukes
         public async Task<IActionResult> Grid()
         {
+            
             List<Listing> listings = await _listingDbContext.Listings.ToListAsync();
+            if (listings == null)
+            {
+                _logger.LogError("[ListingController] Listing is not found");
+                return NotFound("Listings not found");
+            }
             var listingListViewModel = new ListingListViewModel(listings, "Grid");
             return View(listingListViewModel);
         }
@@ -57,6 +70,7 @@ namespace Forum.Controllers
                 _listingDbContext.Listings.Add(listing);
                 await _listingDbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Table));
+                _logger.LogWarning("[ListingController] Listings creation failed {@listing}", listing);
             }
             return View();
         }
--- Forum/Forum/Program.cs
+++ Forum/Forum/Program.cs
@@ -1,5 +1,7 @@
 using Forum.Models;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,16 @@ builder.Services.AddDbContext<ListingDbContext>(options => {
     options.UseSqlite(
         builder.Configuration["ConnectionStrings:ListingDbContextConnection"]);
 });
+
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information() // Levels : Trace < Information < Warning < Error < Fatal
+    .WriteTo.File($"Logs/app_{DateTime.Now:yyyyMMdd_HHmmss}.log");
+
+loggerConfiguration.Filter.ByExcluding(e => e.Properties.TryGetValue("SourceContext", out var value) && e.Level == LogEventLevel.Information &&
+    e.MessageTemplate.Text.Contains("Executed DbCommand"));
+
+var logger = loggerConfiguration.CreateLogger();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
