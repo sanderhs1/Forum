6879a275881a4441d683c0d2dbeee53f9f1d2180
sanderhs1 <91096641+sanderhs1@users.noreply.github.com>
2023-10-20 21:36:09 +0200

    vc

--- Forum/Forum/Controllers/ListingController.cs
+++ Forum/Forum/Controllers/ListingController.cs
@@ -25,17 +25,17 @@ public class ListingController : Controller
     //    return ListingRepository.Rents.ToList();
 
     //}
-    
 
-    public async Task<IActionResult>Table()
+
+    public async Task<IActionResult> Table()
     {
-        
+
         var listings = await _listingRepository.GetAll();
         if (listings == null)
         {
             _logger.LogError("[ListingController] Listing list not found while executing _lisitngRepository.GetAll()");
             return NotFound("Listings list not found");
-                
+
         }
         var listingListViewModel = new ListingListViewModel(listings, "Table");
         return View(listingListViewModel);
@@ -62,7 +62,7 @@ public class ListingController : Controller
             _logger.LogError("[ListingController] Listing not found for the ListingId {ListingId: 0000}", id);
             return NotFound("Listings list not found for the ListingId");
         }
-            
+
         return View(listing);
     }
 
@@ -75,7 +75,7 @@ public class ListingController : Controller
 
     [Authorize]
     [HttpPost]
-    public async Task<IActionResult>Create(Listing listing)
+    public async Task<IActionResult> Create(Listing listing)
     {
         if (ModelState.IsValid)
         {
@@ -89,10 +89,10 @@ public class ListingController : Controller
 
     [Authorize]
     [HttpGet]
-    public async Task<IActionResult>Update(int id)
+    public async Task<IActionResult> Update(int id)
     {
         var listing = await _listingRepository.GetListingById(id);
-        if(listing == null)
+        if (listing == null)
         {
             _logger.LogError("[ListingController] Listing not found when updating the ListingId{ListingId:0000}", id);
             return BadRequest("Listing not found for the ListingId");
@@ -102,7 +102,7 @@ public class ListingController : Controller
 
     [Authorize]
     [HttpPost]
-    public async Task<IActionResult>Update(Listing listing)
+    public async Task<IActionResult> Update(Listing listing)
     {
         if (ModelState.IsValid)
         {
@@ -116,7 +116,7 @@ public class ListingController : Controller
 
     [Authorize]
     [HttpGet]
-    public async Task<IActionResult>Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
         var listing = await _listingRepository.GetListingById(id);
         if (listing == null)
@@ -129,7 +129,7 @@ public class ListingController : Controller
 
     [Authorize]
     [HttpPost]
-    public async Task<IActionResult>DeleteConfirmed(int id)
+    public async Task<IActionResult> DeleteConfirmed(int id)
     {
         bool returnOk = await _listingRepository.Delete(id);
         if (!returnOk)
@@ -139,32 +139,6 @@ public class ListingController : Controller
         }
         return RedirectToAction(nameof(Table));
     }
-
-    services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-{
-    // Configuring password
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequiredLength = 8;
-
-    // Configuring lockout settings
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(60);
-
-    // Each user requires a unique email
-    options.User.RequireUniqueEmail = true;
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
-
-services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Setting the idle timeout for the session
-    options.Cookie.Name = ".AdventureWorks.Session"; // Setting the name of the session cookie
-    options.Cookie.IsEssential = true; // Indicating whether the session cookie is essential
-});
 
 }
 
