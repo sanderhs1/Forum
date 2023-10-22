d139fca438669e76ec9c13f8e02950e7475edef3
sanderhs1 <91096641+sanderhs1@users.noreply.github.com>
2023-10-22 20:39:38 +0200

    RentController

---
+++ Forum/Forum/Controllers/RentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers
+{
+    public class RentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
