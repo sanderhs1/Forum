c21e69d0f630ffaa8b324d7a44b799c233a62456
Tony Tuong Nguyen <s341492@oslomet.no>
2023-10-23 23:10:22 +0200

    Lagt til ListingControllerTests.cs men ikke klart å fullføre denne

---
+++ Forum/xUnitTestForum/Controllers/ListingControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Forum.Controllers;
+using Forum.DAL;
+using Forum.Models;
+using Forum.ViewModels;
+
+
+namespace xUnitTestForum.Controllers
+{
+    public class ListingControllerTests
+    {
+        [Fact]
+        public async Task TestTable()
+        {
+            // arrange
+            var itemList = new List<Listing>();
+            new Listing
+            {
+                ListingId = 1,
+                Name ="test",
+                Price = 500,
+                Description = "House for renting",
+
+              
+            }
+        }
+    }
+}
