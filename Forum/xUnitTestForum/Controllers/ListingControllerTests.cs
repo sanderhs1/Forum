c854e5f5b052ee58425d64ab49bc8adf5d941567
s356291 <89486176+s356291@users.noreply.github.com>
2023-10-24 13:21:59 +0200

    Opprettet ferdig Listing Controller Test

--- Forum/xUnitTestForum/Controllers/ListingControllerTests.cs
+++ Forum/xUnitTestForum/Controllers/ListingControllerTests.cs
@@ -7,24 +7,52 @@ using Forum.Models;
 using Forum.ViewModels;
 
 
-namespace xUnitTestForum.Controllers
+namespace xUnitTestForum.Controllers;
+
+public class ListingControllerTests
 {
-    public class ListingControllerTests
+    [Fact]
+    public async Task TestTable()
     {
-        [Fact]
-        public async Task TestTable()
+        // arrange
+        var listingList = new List<Listing>()
         {
-            // arrange
-            var itemList = new List<Listing>();
             new Listing
             {
                 ListingId = 1,
-                Name ="test",
-                Price = 500,
-                Description = "House for renting",
-
-              
+                Name ="Nice house",
+                Price = 400,
+                Description = "Nice house with windows",
+                ImageUrl = "/images/Hus1.jpg"
+            },
+            new Listing
+            {
+                ListingId = 2,
+                Name = "Bad House",
+                Price = 100,
+                Description="Bad house without windows",
+                ImageUrl = "/images/Kitchen1.jpg"
             }
-        }
+        };
+
+        var mockListingRepository = new Mock<InterListingRepository>();
+        mockListingRepository.Setup(repo => repo.GetAll()).ReturnAsync(listingList);
+        var mockLogger = new Mock<ILogger<ListingController>>();
+        var listingCOntroller = new ListingControllerTests(mockListingRepository.Object, mockLogger.Object);
+
+
+
+        // Act
+
+        var result = await listingCOntroller.Table();
+
+
+        // Assert
+
+
+        var viewResult = Assert.IsType<viewResult>(result);
+        var listingListViewModel = Assert.IsAssignableFrom<ListingListViewModel>(viewResult.ViewData.Model);
+        Assert.Equal(2, listingListViewModel.Listings.Count());
+        Assert.Equal(listingList, listingListViewModel.Listings);
     }
 }
