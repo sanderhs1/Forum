735f7a6677d6e81435bed6a694bfc15a50ae7238
Soban Ali <alisoban27@outlook.com>
2023-10-30 13:03:16 +0100

    Model for Bad, Adresse, osv

--- Forum/Forum/Models/Listing.cs
+++ Forum/Forum/Models/Listing.cs
@@ -10,7 +10,12 @@ namespace Forum.Models
         public int ListingId { get; set; }
 
         [Range(1, 100, ErrorMessage = "Number of rooms should be between 1 and 100.")]
-        public int AntallRom { get; set; }
+        /*public int AntallRom { get; set; }
+        public int Bad { get; set; }
+        public int Area { get; set; }
+        public int Floor { get; set; }
+        public int Spots { get; set; }
+        public string? Adresse { get; set; }*/
 
         [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}", ErrorMessage = "The Name must be numbers or letters and between 2 to 20 characters.")]
         [Display(Name = "Item name")]
@@ -25,9 +30,6 @@ namespace Forum.Models
         public string? ImageUrl { get; set; }
 
         public string? ImageUrl1 { get; set; }
-
-
-
 
 
         public virtual List<RentListing>? RentListings { get; set; }
