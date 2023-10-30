8332242e2abe87197a3e7f0f35ffaff5c8dcc2c9
sanderhs1 <s362067@oslomet.no>
2023-10-30 13:14:40 +0100

    db

---
+++ Forum/Forum/Migrations/20231030121401_AddRoomsToListings.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomsToListings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adresse",
+                table: "Listings",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Area",
+                table: "Listings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Bad",
+                table: "Listings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Floor",
+                table: "Listings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Spots",
+                table: "Listings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adresse",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Bad",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Floor",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Spots",
+                table: "Listings");
+        }
+    }
+}
--- Forum/Forum/Models/Listing.cs
+++ Forum/Forum/Models/Listing.cs
@@ -10,12 +10,12 @@ namespace Forum.Models
         public int ListingId { get; set; }
 
         [Range(1, 100, ErrorMessage = "Number of rooms should be between 1 and 100.")]
-        /*public int AntallRom { get; set; }
+        public int AntallRom { get; set; }
         public int Bad { get; set; }
         public int Area { get; set; }
         public int Floor { get; set; }
         public int Spots { get; set; }
-        public string? Adresse { get; set; }*/
+        public string? Adresse { get; set; }
 
         [RegularExpression(@"[0-9a-zA-ZæøåÆØÅ. \-]{2,20}", ErrorMessage = "The Name must be numbers or letters and between 2 to 20 characters.")]
         [Display(Name = "Item name")]
